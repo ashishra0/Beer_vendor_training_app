0fc4349d8bf0ff0642b6c7109e1ba183b01bb565
Ashish Rao <ashishrao2598@gmail.com>
2018-05-08 20:11:12 +0530

    initial commit

--- lib/beer_vendor_app.rb
+++ lib/beer_vendor_app.rb
@@ -2,8 +2,21 @@ require 'json'
 
 class BeerVendorApp
 
-  def self.call prices_json, orders_json, payments_json
+    prices_json = File.read('data/price_list.json')
+    orders_json = File.read('data/orders.json')
+    payments_json = File.read('data/payments.json')
 
+    prices_json_hash = JSON.parse(prices_json)
+    orders_json_hash = JSON.parse(orders_json)    
+    payments_json_hash = JSON.parse(payments_json)
+
+  def self.call prices_json, orders_json, payments_json
+    
+  end
+
+  def each_user_total_cost
+    beer_price = prices_json_hash.collect {|p| "#{p['prices']}" }
+    beer_order = orders_json_hash.collect {|o| "#{o['size']} #{o['drink']}"}
   end
 
 end
