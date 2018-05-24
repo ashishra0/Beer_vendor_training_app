366aa44b85c2d92ddb2e4bf6b35a75574b2fa1fc
Ashish Rao <ashishrao2598@gmail.com>
2018-05-24 15:13:24 +0530

    solved all the objectives

--- lib/beer_vendor_app.rb
+++ lib/beer_vendor_app.rb
@@ -8,6 +8,8 @@ class BeerVendorApp
     payments_list = JSON.parse(payments_json)
     prices_list = JSON.parse(price_list_json)
     # binding.pry
+
+    # each users's orders.
     user_orders = []
     orders_list.each do |order|
       # binding.pry
@@ -15,8 +17,8 @@ class BeerVendorApp
         item = order["drink"] + " " + order["size"]
         user_orders  << {users => item}
     end
-      puts user_orders
 
+    #  price list of each item.
     price_list = []
     prices_list.each do |price|
         drink_name = price["drink_name"]
@@ -27,11 +29,12 @@ class BeerVendorApp
             drink_name = price["drink_name"]
         end
     end
-    puts price_list
-		
-		result = {}
+    
+    # total cost of each user's orders.
+    result = {}
 		user_orders.each do |elem|
       price_list.each do |price|
+        # binding.pry
         if price.keys.first == elem.values.first
           if result.has_key?(elem.keys.first)
 						result[elem.keys.first] = result[elem.keys.first] + price.values.first
@@ -41,7 +44,29 @@ class BeerVendorApp
         end
 			end
     end
-    binding.pry
-		puts result     
+    puts result
+       
+  # total payment by each user.
+    payment_list = {}
+    payments_list.each do |amt|
+        # binding.pry
+      if payment_list.has_key?(amt.values.first)
+        payment_list[amt.values.first] = amt.values.last + amt.values.last
+      else
+        payment_list.merge!({amt.values.first => amt.values.last})
+      end
+    end
+    puts payment_list
+
+    # What each user owes
+    owes_list = {}
+    dues = payment_list.merge result.select {|k| payment_list.keys.include? k }
+    owes_list = payment_list.merge(dues) do |k, v1, v2|
+      if payment_list.has_key?(k)
+        v2-v1
+      end
+    end
+    # binding.pry
+    puts owes_list
   end
 end
